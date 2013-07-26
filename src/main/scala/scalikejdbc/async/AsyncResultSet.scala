51047218aad9cf92266f001b3eee504d45c0710a
Kazuhiro Sera <seratch@gmail.com>
2013-07-26 09:02:08 +0900

    Moved cast-based implementations to mauricio area

--- src/main/scala/scalikejdbc/async/AsyncResultSet.scala
+++ src/main/scala/scalikejdbc/async/AsyncResultSet.scala
@@ -38,21 +38,8 @@ trait AsyncResultSet extends WrappedResultSet {
   override def asciiStream(columnIndex: Int): java.io.InputStream = throw new UnsupportedOperationException
   override def asciiStream(columnLabel: String): java.io.InputStream = throw new UnsupportedOperationException
 
-  override def bigDecimal(columnIndex: Int): java.math.BigDecimal = any(columnIndex) match {
-    case null => null
-    case bd: java.math.BigDecimal => bd
-    case bd: scala.BigDecimal => bd.underlying
-    case str: String => new java.math.BigDecimal(str)
-    case any => new java.math.BigDecimal(any.toString)
-  }
-
-  override def bigDecimal(columnLabel: String): java.math.BigDecimal = any(columnLabel) match {
-    case null => null
-    case bd: java.math.BigDecimal => bd
-    case bd: scala.BigDecimal => bd.underlying
-    case str: String => new java.math.BigDecimal(str)
-    case any => new java.math.BigDecimal(any.toString)
-  }
+  override def bigDecimal(columnIndex: Int): java.math.BigDecimal
+  override def bigDecimal(columnLabel: String): java.math.BigDecimal
 
   override def binaryStream(columnIndex: Int): java.io.InputStream = throw new UnsupportedOperationException
   override def binaryStream(columnLabel: String): java.io.InputStream = throw new UnsupportedOperationException
@@ -60,15 +47,8 @@ trait AsyncResultSet extends WrappedResultSet {
   override def blob(columnIndex: Int): java.sql.Blob = throw new UnsupportedOperationException
   override def blob(columnLabel: String): java.sql.Blob = throw new UnsupportedOperationException
 
-  override def bytes(columnIndex: Int): Array[Byte] = any(columnIndex) match {
-    case null => null
-    case any => any.asInstanceOf[Array[Byte]]
-  }
-
-  override def bytes(columnLabel: String): Array[Byte] = any(columnLabel) match {
-    case null => null
-    case any => any.asInstanceOf[Array[Byte]]
-  }
+  override def bytes(columnIndex: Int): Array[Byte]
+  override def bytes(columnLabel: String): Array[Byte]
 
   override def characterStream(columnIndex: Int): java.io.Reader = throw new UnsupportedOperationException
   override def characterStream(columnLabel: String): java.io.Reader = throw new UnsupportedOperationException
@@ -79,33 +59,8 @@ trait AsyncResultSet extends WrappedResultSet {
   override def concurrency: Int = throw new UnsupportedOperationException
   override def cursorName: String = throw new UnsupportedOperationException
 
-  override def date(columnIndex: Int): java.sql.Date = any(columnIndex) match {
-    case null => null
-    case d: java.sql.Date => d
-    case t: java.sql.Time => new java.sql.Date(t.getTime)
-    case t: java.sql.Timestamp => new java.sql.Date(t.getTime)
-    case ldt: LocalDateTime => new java.sql.Date(ldt.toDateTime.getMillis)
-    case dt: DateTime => new java.sql.Date(dt.getMillis)
-    case t: LocalTime => t.toSqlTimestamp.toSqlDate
-    case dt: java.util.Date => new java.sql.Date(dt.getTime)
-    case fd: FiniteDuration => new java.sql.Date(fd.toMillis)
-    case other => throw new UnsupportedOperationException(
-      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #date(Int)!")
-  }
-
-  override def date(columnLabel: String): java.sql.Date = any(columnLabel) match {
-    case null => null
-    case d: java.sql.Date => d
-    case t: java.sql.Time => new java.sql.Date(t.getTime)
-    case t: java.sql.Timestamp => new java.sql.Date(t.getTime)
-    case ldt: LocalDateTime => new java.sql.Date(ldt.toDateTime.getMillis)
-    case dt: DateTime => new java.sql.Date(dt.getMillis)
-    case t: LocalTime => t.toSqlTimestamp.toSqlDate
-    case dt: java.util.Date => new java.sql.Date(dt.getTime)
-    case fd: FiniteDuration => new java.sql.Date(fd.toMillis)
-    case other => throw new UnsupportedOperationException(
-      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #date(String)!")
-  }
+  override def date(columnIndex: Int): java.sql.Date
+  override def date(columnLabel: String): java.sql.Date
 
   override def date(columnIndex: Int, cal: Calendar): java.sql.Date = throw new UnsupportedOperationException
   override def date(columnLabel: String, cal: Calendar): java.sql.Date = throw new UnsupportedOperationException
@@ -124,8 +79,8 @@ trait AsyncResultSet extends WrappedResultSet {
   override def nString(columnIndex: Int): String = throw new UnsupportedOperationException
   override def nString(columnLabel: String): String = throw new UnsupportedOperationException
 
-  override def any(columnIndex: Int): Any = throw new IllegalStateException("This method should be implemented.")
-  override def any(columnLabel: String): Any = throw new IllegalStateException("This method should be implemented.")
+  override def any(columnIndex: Int): Any
+  override def any(columnLabel: String): Any
 
   override def any(columnIndex: Int, map: Map[String, Class[_]]): Any = throw new UnsupportedOperationException
   override def any(columnLabel: String, map: Map[String, Class[_]]): Any = throw new UnsupportedOperationException
@@ -142,93 +97,23 @@ trait AsyncResultSet extends WrappedResultSet {
 
   override def statement: java.sql.Statement = throw new UnsupportedOperationException
 
-  override def string(columnIndex: Int): String = any(columnIndex) match {
-    case null => null
-    case str: String => str
-    case any => any.toString
-  }
+  override def string(columnIndex: Int): String
+  override def string(columnLabel: String): String
 
-  override def string(columnLabel: String): String = any(columnLabel) match {
-    case null => null
-    case str: String => str
-    case any => any.toString
-  }
-
-  override def time(columnIndex: Int): java.sql.Time = any(columnIndex) match {
-    case null => null
-    case t: java.sql.Time => t
-    case d: java.sql.Date => new java.sql.Time(d.getTime)
-    case t: java.sql.Timestamp => new java.sql.Time(t.getTime)
-    case t: LocalTime => t.toSqlTime
-    case ldt: LocalDateTime => new java.sql.Time(ldt.toDateTime.getMillis)
-    case dt: DateTime => new java.sql.Time(dt.getMillis)
-    case dt: java.util.Date => new java.sql.Time(dt.getTime)
-    case fd: FiniteDuration => new java.sql.Time(fd.toMillis)
-    case other => throw new UnsupportedOperationException(
-      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #time(Int)!")
-  }
-
-  override def time(columnLabel: String): java.sql.Time = any(columnLabel) match {
-    case null => null
-    case t: java.sql.Time => t
-    case d: java.sql.Date => new java.sql.Time(d.getTime)
-    case t: java.sql.Timestamp => new java.sql.Time(t.getTime)
-    case t: LocalTime => t.toSqlTime
-    case ldt: LocalDateTime => new java.sql.Time(ldt.toDateTime.getMillis)
-    case dt: DateTime => new java.sql.Time(dt.getMillis)
-    case d: java.util.Date => new java.sql.Time(d.getTime)
-    case fd: FiniteDuration => new java.sql.Time(fd.toMillis)
-    case other => throw new UnsupportedOperationException(
-      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #time(String)!")
-  }
+  override def time(columnIndex: Int): java.sql.Time
+  override def time(columnLabel: String): java.sql.Time
 
   override def time(columnIndex: Int, cal: Calendar): java.sql.Time = throw new UnsupportedOperationException
   override def time(columnLabel: String, cal: Calendar): java.sql.Time = throw new UnsupportedOperationException
 
-  override def timestamp(columnIndex: Int): java.sql.Timestamp = any(columnIndex) match {
-    case null => null
-    case t: java.sql.Timestamp => t
-    case t: java.sql.Time => new java.sql.Timestamp(t.getTime)
-    case d: java.sql.Date => new java.sql.Timestamp(d.getTime)
-    case ldt: LocalDateTime => new java.sql.Timestamp(ldt.toDate.getTime)
-    case dt: DateTime => new java.sql.Timestamp(dt.getMillis)
-    case t: LocalTime => t.toSqlTimestamp
-    case d: java.util.Date => new java.sql.Timestamp(d.getTime)
-    case fd: FiniteDuration => new java.sql.Timestamp(fd.toMillis)
-    case other => throw new UnsupportedOperationException(
-      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #timestamp(Int)!")
-  }
-
-  override def timestamp(columnLabel: String): java.sql.Timestamp = any(columnLabel) match {
-    case null => null
-    case t: java.sql.Timestamp => t
-    case t: java.sql.Time => new java.sql.Timestamp(t.getTime)
-    case d: java.sql.Date => new java.sql.Timestamp(d.getTime)
-    case ldt: LocalDateTime => new java.sql.Timestamp(ldt.toDate.getTime)
-    case dt: DateTime => new java.sql.Timestamp(dt.getMillis)
-    case t: LocalTime => t.toSqlTimestamp
-    case d: java.util.Date => new java.sql.Timestamp(d.getTime)
-    case fd: FiniteDuration => new java.sql.Timestamp(fd.toMillis)
-    case other => throw new UnsupportedOperationException(
-      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #timestamp(String)!")
-  }
+  override def timestamp(columnIndex: Int): java.sql.Timestamp
+  override def timestamp(columnLabel: String): java.sql.Timestamp
 
   override def timestamp(columnIndex: Int, cal: Calendar): java.sql.Timestamp = throw new UnsupportedOperationException
   override def timestamp(columnLabel: String, cal: Calendar): java.sql.Timestamp = throw new UnsupportedOperationException
 
-  override def url(columnIndex: Int): java.net.URL = any(columnIndex) match {
-    case null => null
-    case url: java.net.URL => url
-    case str: String => new java.net.URL(str)
-    case any => new java.net.URL(any.toString)
-  }
-
-  override def url(columnLabel: String): java.net.URL = any(columnLabel) match {
-    case null => null
-    case url: java.net.URL => url
-    case str: String => new java.net.URL(str)
-    case any => new java.net.URL(any.toString)
-  }
+  override def url(columnIndex: Int): java.net.URL
+  override def url(columnLabel: String): java.net.URL
 
   override def warnings: java.sql.SQLWarning = throw new UnsupportedOperationException
 
--- src/main/scala/scalikejdbc/async/internal/MauricioAsyncResultSet.scala
+++ src/main/scala/scalikejdbc/async/internal/MauricioAsyncResultSet.scala
@@ -15,9 +15,12 @@
  */
 package scalikejdbc.async.internal
 
+import scalikejdbc._
 import com.github.mauricio.async.db.RowData
 import scalikejdbc.WrappedResultSet
 import scalikejdbc.async.AsyncResultSet
+import org.joda.time.{ LocalTime, DateTime, LocalDateTime }
+import scala.concurrent.duration.FiniteDuration
 
 /**
  * ResultSet implementation
@@ -29,11 +32,149 @@ private[scalikejdbc] class MauricioAsyncResultSet(rows: IndexedSeq[RowData])
   private[this] val currentRow: Option[RowData] = rows.headOption
 
   // AsyncResultSet API
+
   override def next(): Boolean = rows.headOption.isDefined
   override def tail(): AsyncResultSet = new MauricioAsyncResultSet(rows.tail)
 
   // WrappedResultSet API
+
   override def any(columnIndex: Int): Any = currentRow.map(_.apply(columnIndex)).orNull[Any]
   override def any(columnLabel: String): Any = currentRow.map(_.apply(columnLabel)).orNull[Any]
+
+  override def bigDecimal(columnIndex: Int): java.math.BigDecimal = any(columnIndex) match {
+    case null => null
+    case bd: java.math.BigDecimal => bd
+    case bd: scala.BigDecimal => bd.underlying
+    case str: String => new java.math.BigDecimal(str)
+    case any => new java.math.BigDecimal(any.toString)
+  }
+
+  override def bigDecimal(columnLabel: String): java.math.BigDecimal = any(columnLabel) match {
+    case null => null
+    case bd: java.math.BigDecimal => bd
+    case bd: scala.BigDecimal => bd.underlying
+    case str: String => new java.math.BigDecimal(str)
+    case any => new java.math.BigDecimal(any.toString)
+  }
+
+  override def bytes(columnIndex: Int): Array[Byte] = any(columnIndex) match {
+    case null => null
+    case any => any.asInstanceOf[Array[Byte]]
+  }
+
+  override def bytes(columnLabel: String): Array[Byte] = any(columnLabel) match {
+    case null => null
+    case any => any.asInstanceOf[Array[Byte]]
+  }
+
+  override def date(columnIndex: Int): java.sql.Date = any(columnIndex) match {
+    case null => null
+    case d: java.sql.Date => d
+    case t: java.sql.Time => new java.sql.Date(t.getTime)
+    case t: java.sql.Timestamp => new java.sql.Date(t.getTime)
+    case ldt: LocalDateTime => new java.sql.Date(ldt.toDateTime.getMillis)
+    case dt: DateTime => new java.sql.Date(dt.getMillis)
+    case t: LocalTime => t.toSqlTimestamp.toSqlDate
+    case dt: java.util.Date => new java.sql.Date(dt.getTime)
+    case fd: FiniteDuration => new java.sql.Date(fd.toMillis)
+    case other => throw new UnsupportedOperationException(
+      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #date(Int)!")
+  }
+
+  override def date(columnLabel: String): java.sql.Date = any(columnLabel) match {
+    case null => null
+    case d: java.sql.Date => d
+    case t: java.sql.Time => new java.sql.Date(t.getTime)
+    case t: java.sql.Timestamp => new java.sql.Date(t.getTime)
+    case ldt: LocalDateTime => new java.sql.Date(ldt.toDateTime.getMillis)
+    case dt: DateTime => new java.sql.Date(dt.getMillis)
+    case t: LocalTime => t.toSqlTimestamp.toSqlDate
+    case dt: java.util.Date => new java.sql.Date(dt.getTime)
+    case fd: FiniteDuration => new java.sql.Date(fd.toMillis)
+    case other => throw new UnsupportedOperationException(
+      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #date(String)!")
+  }
+
+  override def string(columnIndex: Int): String = any(columnIndex) match {
+    case null => null
+    case str: String => str
+    case any => any.toString
+  }
+
+  override def string(columnLabel: String): String = any(columnLabel) match {
+    case null => null
+    case str: String => str
+    case any => any.toString
+  }
+
+  override def time(columnIndex: Int): java.sql.Time = any(columnIndex) match {
+    case null => null
+    case t: java.sql.Time => t
+    case d: java.sql.Date => new java.sql.Time(d.getTime)
+    case t: java.sql.Timestamp => new java.sql.Time(t.getTime)
+    case t: LocalTime => t.toSqlTime
+    case ldt: LocalDateTime => new java.sql.Time(ldt.toDateTime.getMillis)
+    case dt: DateTime => new java.sql.Time(dt.getMillis)
+    case dt: java.util.Date => new java.sql.Time(dt.getTime)
+    case fd: FiniteDuration => new java.sql.Time(fd.toMillis)
+    case other => throw new UnsupportedOperationException(
+      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #time(Int)!")
+  }
+
+  override def time(columnLabel: String): java.sql.Time = any(columnLabel) match {
+    case null => null
+    case t: java.sql.Time => t
+    case d: java.sql.Date => new java.sql.Time(d.getTime)
+    case t: java.sql.Timestamp => new java.sql.Time(t.getTime)
+    case t: LocalTime => t.toSqlTime
+    case ldt: LocalDateTime => new java.sql.Time(ldt.toDateTime.getMillis)
+    case dt: DateTime => new java.sql.Time(dt.getMillis)
+    case d: java.util.Date => new java.sql.Time(d.getTime)
+    case fd: FiniteDuration => new java.sql.Time(fd.toMillis)
+    case other => throw new UnsupportedOperationException(
+      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #time(String)!")
+  }
+
+  override def timestamp(columnIndex: Int): java.sql.Timestamp = any(columnIndex) match {
+    case null => null
+    case t: java.sql.Timestamp => t
+    case t: java.sql.Time => new java.sql.Timestamp(t.getTime)
+    case d: java.sql.Date => new java.sql.Timestamp(d.getTime)
+    case ldt: LocalDateTime => new java.sql.Timestamp(ldt.toDate.getTime)
+    case dt: DateTime => new java.sql.Timestamp(dt.getMillis)
+    case t: LocalTime => t.toSqlTimestamp
+    case d: java.util.Date => new java.sql.Timestamp(d.getTime)
+    case fd: FiniteDuration => new java.sql.Timestamp(fd.toMillis)
+    case other => throw new UnsupportedOperationException(
+      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #timestamp(Int)!")
+  }
+
+  override def timestamp(columnLabel: String): java.sql.Timestamp = any(columnLabel) match {
+    case null => null
+    case t: java.sql.Timestamp => t
+    case t: java.sql.Time => new java.sql.Timestamp(t.getTime)
+    case d: java.sql.Date => new java.sql.Timestamp(d.getTime)
+    case ldt: LocalDateTime => new java.sql.Timestamp(ldt.toDate.getTime)
+    case dt: DateTime => new java.sql.Timestamp(dt.getMillis)
+    case t: LocalTime => t.toSqlTimestamp
+    case d: java.util.Date => new java.sql.Timestamp(d.getTime)
+    case fd: FiniteDuration => new java.sql.Timestamp(fd.toMillis)
+    case other => throw new UnsupportedOperationException(
+      s"Please send a feedback to the library maintainers about supporting ${other.getClass} for #timestamp(String)!")
+  }
+
+  override def url(columnIndex: Int): java.net.URL = any(columnIndex) match {
+    case null => null
+    case url: java.net.URL => url
+    case str: String => new java.net.URL(str)
+    case any => new java.net.URL(any.toString)
+  }
+
+  override def url(columnLabel: String): java.net.URL = any(columnLabel) match {
+    case null => null
+    case url: java.net.URL => url
+    case str: String => new java.net.URL(str)
+    case any => new java.net.URL(any.toString)
+  }
 
 }
