808e34b043b483d4e33856a9c39ef16afcb2e468
Kazuhiro Sera <seratch@gmail.com>
2014-05-03 22:28:33 +0900

    Fixed typo

--- project/Build.scala
+++ project/Build.scala
@@ -123,7 +123,7 @@ object ScalikeJDBCAsyncProject extends Build {
       <developers>
         <developer>
           <id>seratch</id>
-          <name>Kazuhuiro Sera</name>
+          <name>Kazuhiro Sera</name>
           <url>http://seratch.net/</url>
         </developer>
       </developers>
