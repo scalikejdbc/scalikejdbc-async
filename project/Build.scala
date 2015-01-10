2c371e9a0a55ee7e64cf285810814308ca44bfe7
xuwei-k <6b656e6a69@gmail.com>
2015-01-10 17:16:26 +0700

    0.5.5-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object ScalikeJDBCAsyncProject extends Build {
 
-  lazy val _version = "0.5.4"
+  lazy val _version = "0.5.5-SNAPSHOT"
   lazy val scalikejdbcVersion = "2.2.1"
   lazy val mauricioVersion = "0.2.15"
   lazy val defaultPlayVersion = play.core.PlayVersion.current
